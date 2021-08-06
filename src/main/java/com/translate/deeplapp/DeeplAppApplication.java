e08b1a2abfc7f11b330fccdba8528e9757d3b490
maciek047 <maciek.047@gmail.com>
2021-08-06 02:35:08 +0200

    deepl app init

---
+++ src/main/java/com/translate/deeplapp/DeeplAppApplication.java
@@ -0,0 +1,13 @@
+package com.translate.deeplapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeeplAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeeplAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/translate/deeplapp/DeeplAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.translate.deeplapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeeplAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
